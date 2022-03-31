13a5af54d900cf9ae8726e2332079b3148ccbaf3
bacf5 <brunoagustincarusofassa@gmail.com>
2022-03-31 15:09:24 +0200

    rolling-3d-dice

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const currentDiceItem = document.querySelectorAll('.die-item');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
@@ -27,7 +28,6 @@ const init = function () {
   current0El.textContent = 0;
   current1El.textContent = 0;
 
-  diceEl.classList.add('hidden');
   player0El.classList.remove('player--winner');
   player1El.classList.remove('player--winner');
   player0El.classList.add('player--active');
@@ -43,45 +43,17 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
-// Rolling dice functionality
-btnRoll.addEventListener('click', function () {
-  if (playing) {
-    // 1. Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-
-    // 2. Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
-
-    // 3. Check for rolled 1
-    if (dice !== 1) {
-      // Add dice to current score
-      currentScore += dice;
-      document.getElementById(
-        `current--${activePlayer}`
-      ).textContent = currentScore;
-    } else {
-      // Switch to next player
-      switchPlayer();
-    }
-  }
-});
-
 btnHold.addEventListener('click', function () {
   if (playing) {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore;
-    // scores[1] = scores[1] + currentScore
-
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-
     // 2. Check if player's score is >= 100
     if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
-
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -95,4 +67,32 @@ btnHold.addEventListener('click', function () {
   }
 });
 
+//////////////// new dice roll functionality
+
+function diceRoll() {
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = [...document.querySelectorAll('.die-list')];
+    dice.map((die) => {
+      toggleClasses(die);
+      die.dataset.roll = +Math.trunc(Math.random() * 6) + 1;
+      if (+die.dataset.roll !== 1) {
+        // Add dice to current score
+        currentScore += +die.dataset.roll;
+        document.getElementById(`current--${activePlayer}`).textContent =
+          currentScore;
+      } else {
+        switchPlayer();
+      }
+    });
+  }
+}
+
+// . Rolling effect
+function toggleClasses(die) {
+  die.classList.toggle('odd-roll');
+  die.classList.toggle('even-roll');
+}
+
+btnRoll.addEventListener('click', diceRoll);
 btnNew.addEventListener('click', init);
